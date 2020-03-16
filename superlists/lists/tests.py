4863fcb97a4e312e5ef870241a496484c25f6bfe
Paulo Mol <pmolufmg@gmail.com>
2020-03-16 20:54:48 -0300

    Salvando o POST no Banco de Dados

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -8,17 +8,25 @@ from lists.models import Item
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
+        
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
     
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text':'A new list item'})
+        
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
         
 class ItemModelTest(TestCase):     
     
     def test_saving_and_retrieving_items(self):
+        
         first_item = Item()
         first_item.text = 'O primeiro item'
         first_item.save()
